73bce1e976d395b1b34b13cfe8cfcd6151cf27b2
Chika <chikaokafor94@gmail.com>
2020-06-29 20:01:30 +0100

    quick-sort

--- sorting-algorithms.ts
+++ sorting-algorithms.ts
@@ -110,6 +110,25 @@ class SorthingAlgorithms {
         }
         return result
       }
+
+      quickSort(array) {
+        if(array.length <= 1) {
+          return array;
+        }
+        let pivot = array[array.length - 1];
+        let left = [];
+        let right = [];
+        for(let i = 0; i < array.length - 1; i++) {
+          if(array[i] < pivot) {
+            left.push(array[i])
+          }
+          else {
+            right.push(array[i])
+          }
+        }
+        let arr = [...this.quickSort(left), pivot, ...this.quickSort(right)];
+        return arr;
+      }
   }
   
   let sort = new SorthingAlgorithms();
