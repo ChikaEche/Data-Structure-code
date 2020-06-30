08a7457c71969224856638b54be876812734a458
Chika <chikaokafor94@gmail.com>
2020-06-30 22:54:22 +0100

    A star algorithm

--- A-star-algorithm.ts
+++ A-star-algorithm.ts
@@ -1,8 +1,8 @@
 class PriorityQueue {
     values = [];
-    enqueue(neighbour, distance, visited) {
+    enqueue(neighbour, priority, visited) {
       if(visited.indexOf(neighbour) === -1) {
-        this.values.push({neighbour, distance});
+        this.values.push({neighbour, priority});
       }
       this.sort();
     }
@@ -12,38 +12,48 @@ class PriorityQueue {
     }
   
     sort() {
-      this.values.sort((a,b) => a.distance - b.distance);
+      this.values.sort((a,b) => a.priority - b.priority);
+      this.values = [this.values.shift()];
     }
   }
   
-  class DijkstraAlgorithm {
+  class AStarAlgorithm {
     constructor() {}
     queue = new PriorityQueue();
-    findShortestPath(graph, startNode, endNode) {
+    findShortestPath(graph, startNode, endNode, huestic) {
       let distances = {};
       let visited = [];
       for(let node in graph) {
         distances[node] = {
           previousNode : null,
           distance: Infinity,
+          priority: Infinity,
         };
       }
       distances[startNode] = {
         distance:0,
-        previousNode: null
+        previousNode: null,
+        priority: 0
       }
-      this.queue.enqueue(startNode, distances[startNode].distance, visited)
+      this.queue.enqueue(startNode, distances[startNode].priority, visited)
       visited = [startNode];
       while(this.queue.values.length) {
         let current = this.queue.dequeue();
         current = current.neighbour;
-        
+        console.log(current)
+        if(current === endNode) {
+          break;
+        }
         for(let neighbour in graph[current]){
           let interNodeDistance = distances[current].distance + graph[current][neighbour];
-          if(interNodeDistance < distances[neighbour].distance) {
+          //console.log(interNodeDistance, neighbour)
+          let priority = interNodeDistance + huestic[neighbour];
+          //console.log(priority)
+          if(priority < distances[neighbour].priority) {
+            distances[neighbour].priority = priority;
             distances[neighbour].distance = interNodeDistance;
             distances[neighbour].previousNode = current;
-            this.queue.enqueue(neighbour,interNodeDistance, visited);
+            this.queue.enqueue(neighbour, priority, visited);
             if(visited.indexOf(neighbour) === -1) {
               visited.push(neighbour);
             }
@@ -60,6 +70,7 @@ class PriorityQueue {
         currentNode = distances[currentNode].previousNode
         console.log(`From ${previous} with a total travelled distance of ${distance} to ${currentNode}`)
       }
+      console.log(distances)
     }
   }
   let graph = {
@@ -70,12 +81,13 @@ class PriorityQueue {
     },
     'B': {
       'A':2,
-      'C':1,
+      'C':3,
     },
     'C':{
       'B':1,
       'F':5,
-      'E':3
+      'E':3,
+      
     },
     'D': {
       'F':3,
@@ -93,6 +105,16 @@ class PriorityQueue {
     }
   }
   
-  let path = new DijkstraAlgorithm();
-  path.findShortestPath(graph,'A','C')
+  let huestic = {
+    'A' : 1.5,
+    'B': 4,
+    'C': 2,
+    'D': 0.5,
+    'E': 1.2,
+    'F': 0,
+    'G': 4.5
+  }
+  
+  let path = new AStarAlgorithm();
+  path.findShortestPath(graph,'B','C', huestic)
   
